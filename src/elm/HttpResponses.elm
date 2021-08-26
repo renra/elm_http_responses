bfd1ba4163b67255cd3b0887cf6f4e0c96dcb6d5
Jan Renra Gloser <jan.renra.gloser@gmail.com>
2021-08-26 09:01:24 +0200

    Decoder for user

--- src/elm/HttpResponses.elm
+++ src/elm/HttpResponses.elm
@@ -3,6 +3,23 @@ module HttpResponses exposing(..)
 import Browser
 import Html exposing(Html, div, text)
 import Html.Attributes exposing(class)
+import Json.Decode as Decode
+import Json.Decode.Pipeline
+
+type alias User =
+  { id: Int
+  , name: String
+  }
+
+decodeUsers : Decode.Decoder (List User)
+decodeUsers =
+  Decode.list decodeUser
+
+decodeUser : Decode.Decoder User
+decodeUser =
+  Decode.succeed User
+    |> Json.Decode.Pipeline.required "id" Decode.int
+    |> Json.Decode.Pipeline.required "name" Decode.string
 
 type alias Flags =
   {
@@ -47,4 +64,3 @@ main =
         , subscriptions = subscriptions
         , update = update
         }
-
